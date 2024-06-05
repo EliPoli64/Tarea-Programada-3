57b9882cdebcbbc705eebaf91139e76922a52fa6
EliPoli64 <168288076+EliPoli64@users.noreply.github.com>
2024-06-05 09:06:31 -0600

    Add files via upload

--- funciones.py
+++ funciones.py
@@ -6,10 +6,26 @@ from PIL import ImageTk
 from PIL import Image
 import requests
 from io import BytesIO
-
+from tkinter import messagebox
+import smtplib
+from email.message import EmailMessage
+from clases import *
 #Si un Pokémon atrapado tiene menos de 300 en total de estadísticas, da 300pts. Si está
 #entre 300 y 400 ambos inclusive dan 600pts. Entre 400 y 599 da 900pts, y 600 o más da
 #1800pts.
+
+#Variables globales
+#cantPokeball=0
+#cantSuperball=0
+#cantUltraball=0
+#cantMasterball=0
+#cantBayas=0
+#puntaje=0
+#listaJugador = [300,10,0,0,0,0] #puntaje, pokeball, superball, ultraball, masterball, bayas
+
+
+#Variables globales fin
+
 
 """def tocarCancion():
     playsound.playsound() 
@@ -21,23 +37,167 @@ def grabarBaseEstatica():
     baseEstatica.close()
     return diccPokes
     
-def grabarBaseModificable():
-    pass
+def grabarPersonaje(listaJugador):
+    datosPersonaje=open("datosJugador.txt","w")
+    datosPersonaje.write(str(listaJugador[0])+","+str(listaJugador[1])+","+str(listaJugador[2])+","+
+                         str(listaJugador[3])+","+str(listaJugador[4])+","+str(listaJugador[5]))
+    datosPersonaje.close()
+    return ""
 
+def cargarPersonaje():
+    datosPersonaje=open("datosJugador.txt","r")
+    listaCargada=datosPersonaje.read().split(",")
+    i=0
+    while i<len(listaCargada):
+        listaCargada[i]=int(listaCargada[i])
+        i+=1
+    datosPersonaje.close()
+    return listaCargada
+
+def cargarBaseModificable():
+    listaListas=[]
+    baseMod = open("baseModificable.txt","rb")
+    listaObj=pickle.load(baseMod)
+    for objeto in listaObj:
+        listaListas.append([objeto.obtenerNombre(),objeto.obtenerTuplaMedida(),objeto.obtenerListaTipos(),
+                            objeto.obtenerEsShiny(),objeto.obtenerImagen()])
+    baseMod.close()
+    return listaListas
+
+def grabarBaseModificable(listaCapturados):
+    baseMod = open("baseModificable.txt","wb")
+    pickle.dump(listaCapturados,baseMod)
+    baseMod.close()
+    return ""
+
+def agregarCapturado(tuplaPokemon):
+    listaTipos=[]
+    sumaStats=0
+    infoPoke=pb.pokemon(tuplaPokemon[0])
+    for tipo in infoPoke.types:
+        listaTipos.append(tipo.type.name)
+    for stat in infoPoke.stats:
+        sumaStats+=stat.base_stat
+    if sumaStats<300:
+        puntos=300
+    elif 300<=sumaStats<=400:
+        puntos=600
+    elif 400<sumaStats<599:
+        puntos=900
+    else:
+        puntos=1800
+    if tuplaPokemon[-1]:
+        puntos*=10
+    pokeActual=(tuplaPokemon[0],(infoPoke.height*10,infoPoke.weight*10),listaTipos,puntos)
+    pokemonAtr = Pokemon()
+    pokemonAtr.asignarNombre(pokeActual[0])
+    pokemonAtr.asignarTuplaMedidas(pokeActual[1])
+    pokemonAtr.asignarListaTipos(pokeActual[2])
+    pokemonAtr.asignarPuntos(pokeActual[3])
+    pokemonAtr.asignarEsShiny(tuplaPokemon[-1])
+    sprite=pb.pokemon(pokeActual[0]).sprites
+    if tuplaPokemon[-1]:
+        pokemonAtr.asignarImagen(sprite.front_shiny)
+    else:
+        pokemonAtr.asignarImagen(sprite.front_default)
+    listaCapturados=cargarBaseModificable()
+    listaCapturados.append(pokeActual)
+    grabarBaseModificable(listaCapturados)
+    return ""
+
+def obtenerImagen(nombrePoke,esShiny):
+    sprite=pb.pokemon(nombrePoke).sprites
+    if esShiny:
+        return ImageTk.PhotoImage(Image.open(BytesIO(requests.get(sprite.front_shiny).content)))
+    else:
+        return ImageTk.PhotoImage(Image.open(BytesIO(requests.get(sprite.front_default).content)))
+                            
 def encontrarPokemon(): # diccPokemon[pokemon]=(pokemon,(infoPoke.height*10,infoPoke.weight*10),listaTipos,puntos)
     baseEstatica=open("baseEstatica.txt","rb")
     diccPokes=pickle.load(baseEstatica)
     numPokemon = random.randint(1,len(diccPokes))
-    sprite=pb.pokemon(diccPokes[numPokemon][0]).sprites
-    if random.randint(1,1) ==1:
-        tuplaPokemon=(diccPokes[numPokemon][0],diccPokes[numPokemon][-1]*10,
-                      ImageTk.PhotoImage(Image.open(BytesIO(requests.get(sprite.front_shiny).content))))
+    if random.randint(1,2)==1:
+        tuplaPokemon=(diccPokes[numPokemon][0],diccPokes[numPokemon][-1]*10,True)
     else:
-        tuplaPokemon=(diccPokes[numPokemon][0],diccPokes[numPokemon][-1],
-                      ImageTk.PhotoImage(Image.open(BytesIO(requests.get(sprite.front_default).content)))) 
+        tuplaPokemon=(diccPokes[numPokemon][0],diccPokes[numPokemon][-1],False) 
     baseEstatica.close()
     return tuplaPokemon
 
+def decidirCaptura(pts,index,baya):
+    """
+    Funcionalidad: Decide si el pokémon será capturado con la pokeball seleccionada.
+    Entrada:
+    -pts(int): La cantidad de puntos que vale la captura.
+    -index(int): Indica la pokeball que fue seleccionada.
+    -baya(bool): True si el pokémon ha consumido una baya, False en caso contrario.
+    Salida:
+    -(bool): Determina si el pokemon se atrapará.
+    """
+    captura = False
+    if index == 1 or index == 2 or index == 3:
+        ratioCaptura=index
+    else:
+        ratioCaptura=20
+    if str(pts)[0]=="1":
+        ratioCaptura*=5
+    elif str(pts)[0]=="3":
+        ratioCaptura*=75
+    elif str(pts)[0]=="6":
+        ratioCaptura*=50
+    elif str(pts)[0]=="9":
+        ratioCaptura*=25
+    if baya:
+        ratioCaptura*=1.2
+    if random.randint(1,100)<=ratioCaptura:
+        return True
+    else:
+        return False
+    #Los puntos son directamente proporcionales al ratio de captura. Si da 300pts, el ratio de
+    #captura es de 75, si da 600 es de 50, si da 900 es de 25, y si da 1800 puntos es de 5.
+    #La Pokéball multiplica el ratio de captura por 1, la superball por 2, la ultraball por 3, y la
+    #masterball por 20.
+    #Se genera un número aleatorio de manera que la posibilidad de captura sea acorde a los
+    #ratios dados
+
+def enviarCorreo(nombrePokemon,pts):
+    correo=EmailMessage()
+    correo.set_content("¡Mira! He atrapado este "+nombrePokemon+" y me dio "+str(pts)+" puntos.")
+    correo['Subject'] = "¡He capturado algo! :D"
+    correo["From"]="soggycat64@gmail.com"
+    correo["To"]="soggycat64@gmail.com"
+    server = smtplib.SMTP(host='smtp.gmail.com',port=587)
+    server.starttls()
+    server.set_debuglevel(1)
+    server.login("soggycat64@gmail.com", "afhd rgun lrmy drcv")
+    server.send_message(correo)
+    server.quit()
+    return messagebox.showinfo(title="¡Compartido!",message="Se ha enviado un correo")
+
+def comprarItem(listaJugador,index):
+    if index==1:
+        if listaJugador[0]<100:
+            messagebox.showwarning(title="Falta de brete",message="No tienes suficientes puntos.")
+            return listaJugador
+        listaJugador[0]=listaJugador[0]-100
+    elif index==2 or index==5:
+        if listaJugador[0]<300:
+            messagebox.showwarning(title="Falta de brete",message="No tienes suficientes puntos.")
+            return listaJugador
+        listaJugador[0]=listaJugador[0]-300
+    elif index==3:
+        if listaJugador[0]<900:
+            messagebox.showwarning(title="Falta de brete",message="No tienes suficientes puntos.")
+            return listaJugador
+        listaJugador[0]=listaJugador[0]-900
+    else:
+        if listaJugador[0]<18000:
+            messagebox.showwarning(title="Falta de brete",message="No tienes suficientes puntos.")
+            return listaJugador
+        listaJugador[0]=listaJugador[0]-18000 
+    listaJugador[index]=listaJugador[index]+1
+    grabarPersonaje(listaJugador)
+    return listaJugador
+    
 def obtenerPokes():
     archivo=open("pokemon.txt","r")
     lista=archivo.readline().split(",")
@@ -65,6 +225,3 @@ def obtenerPokes():
     archivo.close()
     return diccPokemon
 
-
-"""def determinarAtrapado(prob,pts):
-    """
--- tareaProgramada3.py
+++ tareaProgramada3.py
@@ -31,16 +31,75 @@ def huirBatalla(interfaz):
     interfaz.destroy()
     return menuPrincipal()
 
-def menuAtrapar(interfaz):
+def opcionCorreo(interfaz,nombrePokemon,pts):
+    if nombrePokemon=="mr-mime":
+        nombrePokemon="Mr. Mime"
+    else:
+        nombrePokemon=nombrePokemon.capitalize()
+    enviarCorreo(nombrePokemon,pts)
+    return salirMenu(interfaz)
+
+def pokemonAtrapado(interfaz,pts,nombrePokemon,tuplaPokemon):
+    interfaz.destroy()
+    interfaz = Tk()
+    listaJugador=cargarPersonaje()
+    listaJugador[0]=listaJugador[0]+pts
+    grabarPersonaje(listaJugador)
+    agregarCapturado(tuplaPokemon)
+    interfaz.resizable(False,False)
+    interfaz.geometry("550x320")
+    interfaz.eval('tk::PlaceWindow . center')
+    fondo=PhotoImage(file="atrapado.png")
+    Label(interfaz, image=fondo).place(x=0, y=0)
+    interfaz.title("¡Atrapado!")
+    if nombrePokemon=="mr-mime":
+        nombrePokemon="Mr. Mime"
+        Label(interfaz, text="Mr. Mime",font=("Arial", 20),bg="#EFE4B0").place(x=40, y=35)
+    else:
+        Label(interfaz, text=nombrePokemon.capitalize(),font=("Arial", 20),bg="#EFE4B0").place(x=40, y=35)
+    Label(interfaz, text="¡"+nombrePokemon.capitalize()+" ha sido capturado!",font=("Arial", 16),
+          bg="#C3C3C3").place(x=15,y=205)
+    Label(interfaz, text=pts,font=("Arial", 20),bg="#EFE4B0").place(x=158, y=76)
+    Button(interfaz, text="Compartir",font=("Arial",20),
+           command=lambda:opcionCorreo(interfaz,nombrePokemon,pts)).place(x=40,y=250)
+    Button(interfaz, text="Salir", font=("Arial",20),
+           command=lambda:salirMenu(interfaz)).place(x=440,y=250)
+    interfaz.mainloop()
+    return ""
+
+def opcionAtrapar(interfaz,nombrePokemon,pts,listaJugador,index,baya,tuplaPokemon):
+    if listaJugador[index]<=0:
+        print("no iohajcb")
+        return messagebox.showwarning(title="No te quedan Pokéballs",message="¡No tienes esas Pokéballs!")
+    listaJugador[index]=listaJugador[index]-1
+    grabarPersonaje(listaJugador)
+    if decidirCaptura(pts,index,baya):
+        messagebox.showinfo(title="¡Ya está!",message="¡Pokémon capturado!")
+        return pokemonAtrapado(interfaz,pts,nombrePokemon,tuplaPokemon)
+    messagebox.showinfo(title="¡Fracaso!",message="¡El Pokémon se ha escapado!")
+    return menuAtrapar(interfaz,baya,tuplaPokemon)
+
+def tirarBaya(interfaz,listaJugador,tuplaPokemon):
+    if listaJugador[5]<=0:
+        return messagebox.showwarning(title="No te quedan Bayas",message="¡No tienes Bayas!")
+    listaJugador[5]=listaJugador[5]-1
+    grabarPersonaje(listaJugador)
+    return menuAtrapar(interfaz,True,tuplaPokemon)
+
+def preAtrapar(interfaz):
     interfaz.destroy()
     interfaz=Tk()
     tuplaPokemon=encontrarPokemon()
+    return menuAtrapar(interfaz,False,tuplaPokemon)
+
+def menuAtrapar(interfaz,baya,tuplaPokemon):
+    listaJugador=cargarPersonaje()
     interfaz.title("Atrapar Pokémon")
     interfaz.resizable(False,False)
     interfaz.geometry("550x320")
     interfaz.eval('tk::PlaceWindow . center') # Coloca la ventana en el centro de la pantalla
     fondo=PhotoImage(file="encuentro.png")
-    imagenPokemon=tuplaPokemon[-1]
+    imagenPokemon=obtenerImagen(tuplaPokemon[0],tuplaPokemon[-1])
     Label(interfaz, image=fondo).place(x=0, y=0)
     if tuplaPokemon[0]=="mr-mime":
         Label(interfaz, text="Mr. Mime",font=("Arial", 20)).place(x=40, y=35)
@@ -53,19 +112,62 @@ def menuAtrapar(interfaz):
     imgUltra=PhotoImage(file="ultra.png")
     imgMaster=PhotoImage(file="master.png")
     imgBaya=PhotoImage(file="baya.png")
+    Label(interfaz, text="Pokéballs: "+str(listaJugador[1]),bg="#C3C3C3").place(x=20,y=210)
+    Label(interfaz, text="Superballs: "+str(listaJugador[2]),bg="#C3C3C3").place(x=20,y=230)
+    Label(interfaz, text="Ultraballs: "+str(listaJugador[3]),bg="#C3C3C3").place(x=20,y=250)
+    Label(interfaz, text="Masterballs: "+str(listaJugador[4]),bg="#C3C3C3").place(x=20,y=270)
+    Label(interfaz, text="Bayas: "+str(listaJugador[5]),bg="#C3C3C3").place(x=20,y=290)
     Button(interfaz, text="Huir",command=lambda:huirBatalla(interfaz),font=("Arial", 20)).place(x=455,y=238)
-    Button(interfaz, image=imgPoke, relief="flat", command=lambda:menuAtrapar()).place(x=300,y=210)
-    Button(interfaz, image=imgSuper, relief="flat", command=lambda:menuAtrapar()).place(x=350,y=210)
-    Button(interfaz, image=imgUltra, relief="flat", command=lambda:menuAtrapar()).place(x=300,y=270)
-    Button(interfaz, image=imgMaster, relief="flat", command=lambda:menuAtrapar()).place(x=350,y=270)
-    Button(interfaz, image=imgBaya, relief="flat", command=lambda:menuAtrapar()).place(x=400,y=240)
+    Button(interfaz, image=imgPoke, relief="flat", 
+           command=lambda:opcionAtrapar(interfaz,tuplaPokemon[0],tuplaPokemon[1],listaJugador,1,baya,tuplaPokemon)).place(x=300,y=210)
+    Button(interfaz, image=imgSuper, relief="flat", 
+           command=lambda:opcionAtrapar(interfaz,tuplaPokemon[0],tuplaPokemon[1],listaJugador,2,baya,tuplaPokemon)).place(x=350,y=210)
+    Button(interfaz, image=imgUltra, relief="flat", 
+           command=lambda:opcionAtrapar(interfaz,tuplaPokemon[0],tuplaPokemon[1],listaJugador,3,baya,tuplaPokemon)).place(x=300,y=270)
+    Button(interfaz, image=imgMaster, relief="flat", 
+           command=lambda:opcionAtrapar(interfaz,tuplaPokemon[0],tuplaPokemon[1],listaJugador,4,baya,tuplaPokemon)).place(x=350,y=270)
+    Button(interfaz, image=imgBaya, relief="flat", 
+           command=lambda:tirarBaya(interfaz,listaJugador,tuplaPokemon)).place(x=400,y=240)
     interfaz.mainloop()
     return
 
 def menuPokedex(interfaz):
+    archivo=open("pokemon.txt","r")
+    lista=archivo.readline().split(",")
+    archivo.close()
+    interfaz.destroy()
+    interfaz = Tk()
+    interfaz.title("Pokédex")
+    interfaz.resizable(True,True)
+    #interfaz.geometry("550x320")
+    interfaz.eval('tk::PlaceWindow . center') # Coloca la ventana en el centro de la pantalla
+    i=1
+    listaPokes=cargarBaseModificable()
+    diccImagenes={}
+    while i <= len(lista):
+        try:
+            diccImagenes[i]=obtenerImagen(listaPokes[i-1][0],False)
+            if listaPokes[i-1][0]=="mr-mime":
+                Label(interfaz, text="Mr. Mime").grid(column=(i-1)%7,row=i//7+2)
+            else:
+                Label(interfaz, text=listaPokes[i-1][0].capitalize()).grid(column=(i-1)%7,row=i//7+2)
+        except:
+            diccImagenes[i]=PhotoImage(file="noEncontrado.png")
+            Label(interfaz, text="No encontrado").grid(column=(i-1)%7,row=i//7+2)
+        Label(interfaz, image = diccImagenes[i]).grid(column=(i-1)%7,row=i//7+1)
+        i+=1
+        print("ksndc")
+    Button(interfaz, text="Salir", font=("Arial",16),
+           command=lambda:salirMenu(interfaz)).place(x=440,y=250)
+    interfaz.mainloop()
     return 
 
+def opcionComprar(interfaz,listaJugador,index):
+    listaJugador = comprarItem(listaJugador, index)
+    return menuTienda(interfaz)
+
 def menuTienda(interfaz):
+    listaJugador=cargarPersonaje()
     interfaz.destroy()
     interfaz=Tk()
     interfaz.resizable(False,False)
@@ -79,15 +181,21 @@ def menuTienda(interfaz):
     imgUltra=PhotoImage(file="ultra.png")
     imgMaster=PhotoImage(file="master.png") 
     imgBaya = PhotoImage(file="baya.png") # Importar imágenes
-    Button(interfaz, image=imgPoke, relief="flat",command=lambda:menuAtrapar()).place(x=112,y=160)
+    Label(interfaz, text=str(listaJugador[0]),bg="#F0D7B7").place(x=362,y=104)
+    Button(interfaz, image=imgPoke, relief="flat",
+                    command=lambda:opcionComprar(interfaz,listaJugador,1)).place(x=112,y=160)
     Label(interfaz, text="Pokéball\nPrecio: 100pts").place(x=95, y=200)   
-    Button(interfaz, image=imgSuper, relief="flat",command=lambda:menuAtrapar()).place(x=262,y=160)
+    Button(interfaz, image=imgSuper, relief="flat",
+                    command=lambda:opcionComprar(interfaz,listaJugador,2)).place(x=262,y=160)
     Label(interfaz,text="Súperball\nPrecio: 300pts").place(x=245, y=200)
-    Button(interfaz, image=imgUltra, relief="flat",command=lambda:menuAtrapar()).place(x=412,y=160)
+    Button(interfaz, image=imgUltra, relief="flat",
+                    command=lambda:opcionComprar(interfaz,listaJugador,3)).place(x=412,y=160)
     Label(interfaz, text= "Ultraball\nPrecio: 900pts").place(x=395, y=200)
-    Button(interfaz, image=imgMaster, relief="flat",command=lambda:menuAtrapar()).place(x=182,y=230)
+    Button(interfaz, image=imgMaster, relief="flat",
+                    command=lambda:opcionComprar(interfaz,listaJugador,4)).place(x=182,y=230)
     Label(interfaz, text="Masterball\nPrecio: 18000pts").place(x=162, y=270)
-    Button(interfaz, image=imgBaya, relief="flat",command=lambda:menuAtrapar()).place(x=342,y=220)
+    Button(interfaz, image=imgBaya, relief="flat",
+                    command=lambda:opcionComprar(interfaz,listaJugador,5)).place(x=342,y=220)
     Label(interfaz, text="Baya\nPrecio: 300pts").place(x=322, y=270)
     Button(interfaz, text="Salir",font=("Arial", 20),
            command=lambda:salirMenu(interfaz)).place(x=460,y=250)
@@ -101,11 +209,20 @@ def menuCreditos(interfaz):
     interfaz.title("Simulador de encuentro")
     interfaz.geometry("550x320")
     interfaz.eval('tk::PlaceWindow . center') # Coloca la ventana en el centro de la pantalla
+    fondo=PhotoImage(file="creditos.png")
     #Créditos con pokefont
-    Label(interfaz, text= "Creado por:\n Elías Ramírez Hernández\nLindsay Nahome Marín Sánchez").place(x=180, y=50)
-    Label(interfaz, text= "Información recopilada de PokeAPI").place(x=180, y=70)
-    Label(interfaz, text= "Sprites de pokéball, superball, ultraball y masterball conseguidos de:"+
-          "\nPokémon Blanco y Negro",).place(x=395, y=80)
+    Label(interfaz, image=fondo).place(x=0, y=0)
+    Label(interfaz,bg="#EFE4B0",font=("Segoe UI",10),
+          text= "Elaborado por Lindsay Nahome Marín Sánchez\ny Elías Ramírez Hernández").place(x=15, y=170)
+    Label(interfaz,bg="#EFE4B0",font=("Segoe UI",10),
+          text= "Información recopilada de PokéAPI").place(x=15, y=200)
+    Label(interfaz,bg="#EFE4B0",font=("Segoe UI",10),
+          text= "Sprites de objetos conseguidos de los archivos de"+
+          "\nPokémon, en internet",).place(x=15, y=110) # Línea dividida 
+    Button(interfaz, text="Salir",
+           command=lambda:salirMenu(interfaz)).place(x=460,y=250)
+    interfaz.mainloop()
+    return ""
 
 def menuPrincipal():
     interfaz=Tk()
@@ -115,16 +232,32 @@ def menuPrincipal():
     interfaz.geometry("550x320")
     interfaz.eval('tk::PlaceWindow . center') # Coloca la ventana en el centro de la pantalla
     Label(interfaz, image=fondo).place(x=0, y=0)
-    Button(interfaz, text="Atrapar", command=lambda:menuAtrapar(interfaz)).place(x=40,y=15)
-    Button(interfaz, text="Pokédex", command=lambda:menuPokedex(interfaz)).place(x=257.5,y=15)
-    Button(interfaz, text="Tienda", command=lambda:menuTienda(interfaz)).place(x=475,y=15)
-    Button(interfaz, text="Créditos", command=lambda:menuCreditos(interfaz)).place(y=280,x=40)
-    Button(interfaz, text="Salir", command=lambda:interfaz.destroy(interfaz)).place(x=475,y=280)
+    Button(interfaz, text="Atrapar",font=("Arial", 20), 
+           command=lambda:preAtrapar(interfaz)).place(x=40,y=15)
+    Button(interfaz, text="Pokédex",font=("Arial", 20), 
+           command=lambda:menuPokedex(interfaz)).place(x=215,y=15)
+    Button(interfaz, text="Tienda",font=("Arial", 20), 
+           command=lambda:menuTienda(interfaz)).place(x=409,y=15)
+    Button(interfaz, text="Créditos",font=("Arial", 20), 
+           command=lambda:menuCreditos(interfaz)).place(x=45,y=250)
+    Button(interfaz, text="Salir",font=("Arial", 20), 
+           command=lambda:interfaz.destroy()).place(x=430,y=250)
     interfaz.mainloop()
     return ""
 
-
 grabarBaseEstatica()
+try:
+    listaListas = cargarBaseModificable()
+except FileNotFoundError:
+    listaListas= grabarBaseModificable([])
+
+try:
+    listaJugador=cargarPersonaje()
+    if listaJugador==[0,0,0,0,0,0]:
+        listaJugador=[300,10,0,0,0,0] # Evita que el jugador se atasque y quede sin recursos
+except:
+    listaJugador=[300,10,0,0,0,0]
+    grabarPersonaje(listaJugador)
 menuPrincipal()
 
 
